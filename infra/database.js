1b14fbec812b1640dbe5fb9f70a122282c07240b
Gabriel Borges <51276652+gabrielborgesweb@users.noreply.github.com>
2024-08-09 01:16:52 +0000

    add a more robust SSL configuration method

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSL(),
   });
 
   console.log("Credenciais do Postgres", {
@@ -16,7 +16,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSL(),
   });
 
   try {
@@ -34,3 +34,14 @@ async function query(queryObject) {
 export default {
   query: query,
 };
+
+// Função para aplicar, se houver, o certificado no SSL.
+function getSSL() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
