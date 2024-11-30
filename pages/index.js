fb2472dc853110b67366b26a9f6029d5b2818c4e
Gabriel Borges <gabrielborgesbonifacio@gmail.com>
2024-11-30 12:25:07 -0300

    feat: creates `/status` frontend page + mini `style.css` refactor

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 function Home() {
   return (
-    <>
+    <div className="frontpage">
       <h1 style={{ marginBottom: "3em" }}>
         Se você estiver lendo isso, me envie um áudio imitando o áudio abaixo:
       </h1>
       <audio src="/audio.mp3" controls />
-    </>
+    </div>
   );
 }
 
